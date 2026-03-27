6e3711613250f16a76ee301f8f564e3dd4906615
Yasmin Ahmed <ya44122037@cihanuniversity.edu.iq>
2026-03-27 16:07:51 +0300

    Fix PORT expansion in Procfile and disable reload

--- edunova-backend/main.py
+++ edunova-backend/main.py
@@ -2280,4 +2280,4 @@ async def serve_spa(full_path: str):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
